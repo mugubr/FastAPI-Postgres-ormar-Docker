888be7319417bc5e689bec5d18e5f91cc45dccd4
mugubr <miguimuniz@gmail.com>
2023-03-21 15:57:18 -0300

    fix: user_services

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -6,34 +6,41 @@ from ..models.user_model import User
 
 router = APIRouter()
 
-@router.post("/users", response_model=User)
+@router.post("/create_an_user", response_model=User, description='Criar Usuário', summary='Criar Usuário')
 async def create_user(user: User):
-    query = User(nome=user.nome, email=user.email, cpf=user.cpf, telefone=user.telefone)
-    await query.save()
-    return query
+    new_user = await User.objects.create(**user.dict(exclude_unset=True))
+    return new_user
 
-@router.get("/users", response_model=List[User])
-async def get_users():
-    query = User.objects.all()
-    return query
 
-@router.get("/users/{user_id}", response_model=User)
-async def get_user(user_id: int):
-    query = await User.objects.get(id=user_id)
-    if not query:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado;")
-    return query
+@router.get("/get_all_users", response_model=List[User], description='Listar todos os usuários', summary='Listar todos os usuários')
+async def read_users():
+    users = await User.objects.all()
+    return users
 
-@router.put("/users/{user_id}")
+
+@router.get("/get_an_user/{user_id}", response_model=User, description='Buscar usuário pelo ID',  summary='Buscar usuário pelo ID')
+async def read_user(user_id: int):
+    user = await User.objects.get_or_none(id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+    return user
+
+
+@router.put("/update_an_user/{user_id}", response_model=User, description='Atualizar dados de um usuário pelo ID', summary='Atualizar dados de um usuário pelo ID')
 async def update_user(user_id: int, user: User):
-    query = await User.objects.get(id=user_id)
-    if not query:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado")
-    await query.update(nome=user.nome, email=user.email)
-    return {"message": "User updated successfully"}
+    current_user = await User.objects.get_or_none(id=user_id)
+    if current_user is None:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+    updated_user = user.dict(exclude_unset=True)
+    for field in updated_user:
+        setattr(current_user, field, updated_user[field])
+    await current_user.update()
+    return current_user
 
-@router.delete("/users/{user_id}")
+
+@router.delete("/delete_an_user/{user_id}", response_model=None, description='Deletar um usuário pelo ID', summary='Deletar um usuário pelo ID')
 async def delete_user(user_id: int):
-    query = await User.objects.get(id=user_id)
-    if not query:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    user = await User.objects.get_or_none(id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+    await user.delete()
